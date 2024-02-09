fbf9e5936e7f1138e2a0f8791113619eb797d22e
Jahkaan Wray <gammawray26@gmail.com>
2024-02-09 18:39:21 +0000

    Switch to python

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <windows.devices.bluetooth.h>
+#include <windows.devices.bluetooth.advertisement.h>
+
+int main() {
+
+    std::cout << "Hello, World!" << std::endl;
+
+
+    //create new device watcher
+    
+
+    return 0;
+}
--- main.py
+++ main.py
@@ -9,17 +9,26 @@ async def main():
         print("Connected: {0}".format(client.is_connected))
         for s in client.services:
             print(s)
-        data = await client.read_gatt_char("00002a65-0000-1000-8000-00805f9b34fb")
+        data = await client.read_gatt_char("00002acc-0000-1000-8000-00805f9b34fb")
         print("Data: {0}".format(data))
         char_uuid = "00002a63-0000-1000-8000-00805f9b34fb"
-        def callback(sender: BleakGATTCharacteristic, data: bytearray):
+        
+        await client.write_gatt_char("00002ad9-0000-1000-8000-00805f9b34fb", bytearray([0x00]))
+        await client.write_gatt_char("00002ad9-0000-1000-8000-00805f9b34fb", bytearray([0x01]))
+        await client.write_gatt_char("00002ad9-0000-1000-8000-00805f9b34fb", bytearray([0x05, 0x64]))
+        def powcallback(sender: BleakGATTCharacteristic, data: bytearray):
             flags = data[0:2]
             power = data[2:4]
             #convert to int
             power = int.from_bytes(power, byteorder='little', signed=False)
             print("Received power: {0}".format(power))
+        
+        def callback(sender: BleakGATTCharacteristic, data: bytearray):
+            print("Received: {0}".format(data))
 
-        await client.start_notify(char_uuid, callback)
+        #await client.start_notify(char_uuid, powcallback)
+        await client.start_notify("00002a5b-0000-1000-8000-00805f9b34fb", callback)
+
     
         await asyncio.sleep(500.0)
     
