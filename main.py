bef91e14efed904235bd5026df1667d957f04a18
Jahkaan Wray <gammawray26@gmail.com>
2024-02-10 16:45:38 +0000

    Target power level

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import asyncio
 from bleak import BleakScanner, BleakClient, BleakGATTCharacteristic
 
+def power_callback(sender, data):
+    flags = data[0:2]
+    power = int.from_bytes(data[2:4], byteorder='little')
+    print(f'Power: {power}')
+
 async def main():
     bleakScanner = BleakScanner()
     await bleakScanner.start()
@@ -31,6 +36,11 @@ async def main():
     print(await client.read_gatt_char('00002a00-0000-1000-8000-00805f9b34fb'))
     print(await client.read_gatt_char('00002ad8-0000-1000-8000-00805f9b34fb'))
     print(await client.read_gatt_char('00002ad6-0000-1000-8000-00805f9b34fb'))
+    await client.write_gatt_char('00002ad9-0000-1000-8000-00805f9b34fb', bytearray([0x00]))
+    await client.write_gatt_char('00002ad9-0000-1000-8000-00805f9b34fb', bytearray([0x01]))
+    await client.write_gatt_char('00002ad9-0000-1000-8000-00805f9b34fb', bytearray([0x05, 0xc8]))
+    await client.start_notify('00002a63-0000-1000-8000-00805f9b34fb', power_callback)
+    await asyncio.sleep(300.0)
     await client.disconnect()
 
 
